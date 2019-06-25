06f87b84d20283a90ad26febed00ad2a9f027ed8
Phelps <phelpsakinsan@gmail.com>
2019-06-25 20:53:49 +0100

    Made changes so the db context would be registered with the Dependency Injection container

--- Sulfur/Startup.cs
+++ Sulfur/Startup.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Sulfur.Models;
 
 namespace Sulfur
 {
@@ -25,6 +27,9 @@ namespace Sulfur
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Register the service with the Dependency Injection Container, DbContext is a service
+            services.AddDbContext<SulfurContext>(opt => opt.UseInMemoryDatabase("UrlPayloadDb"));
+            //Specifies also that it will use an in memory database
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
