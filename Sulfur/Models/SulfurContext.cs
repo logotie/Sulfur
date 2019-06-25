8b513ad5cb7837e293a2abfcdfb0639df7a9af6f
Phelps <phelpsakinsan@gmail.com>
2019-06-25 20:43:38 +0100

    Added DbContext class for db operations

---
+++ Sulfur/Models/SulfurContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sulfur.Models
+{
+    public class SulfurContext : DbContext
+    {
+        //DbContext is primary class responsible for database operations
+        public SulfurContext(DbContextOptions<SulfurContext> options) : base(options)
+        {
+
+        }
+
+        //DbSet acts like a table in the database, collection of UrlPayload objects
+        public DbSet<UrlPayload> UrlPayloads { get; set; }
+    }
+}
