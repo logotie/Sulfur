a625d44e9d47657add6ee19486d9aa654c68c16e
Phelps <phelpsakinsan@gmail.com>
2019-06-27 20:36:36 +0100

    Made changes so the model validates, unit is failing as I need to find a way to mock the http request

--- Sulfur/Controllers/TorrentFileController.cs
+++ Sulfur/Controllers/TorrentFileController.cs
@@ -36,11 +36,18 @@ namespace Sulfur
             string headerAuthToken = Request.Headers[WebConstants.AuthHeaderKeyValue];
 
             //Checks if it is possible to bind the values in the request to the model.
-            //Check also if the authtoken is valid also
+            //If url is null, means it was unable to bind the model
             if (!ModelState.IsValid || !_urlHeaderService.SuccessfulMatchOnHeaderToken(headerAuthToken))
             {
                 //Returns a 400 bad request
                 return BadRequest(ModelState);
+            }
+
+            //Check also if the authtoken is valid also
+            if (!_urlHeaderService.SuccessfulMatchOnHeaderToken(headerAuthToken))
+            {
+                //Returns a 400 bad request
+                return BadRequest();
             }
 
             //ActionResult is the base class for various results for example JSONResult or Result
--- Sulfur/Models/UrlPayload.cs
+++ Sulfur/Models/UrlPayload.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace Sulfur.Models
     //Model Class for storing the url that will accepted by the Post request
     public class UrlPayload
     {
+        //Means the Url field must be present for the model to validate
+        [Required]
         //Model binding is not case sensitive
         //https://docs.microsoft.com/en-us/aspnet/core/mvc/models/model-binding?view=aspnetcore-2.2
         //Added virtual to allow it to be set/get in the unit tests
