71d3d5d95260881fc936b034e8e752676943928a
Phelps <phelpsakinsan@gmail.com>
2019-06-29 22:43:34 +0100

    Added a new test

--- SulfurXunitTests/TorrentFileControllerTests.cs
+++ SulfurXunitTests/TorrentFileControllerTests.cs
@@ -134,5 +134,32 @@ namespace SulfurXunitTests
             //Should be true, as it was able to parse the url and download the file
             Assert.True(boolRepresentationOfString);
         }
+
+
+        //Authorization token should fail
+        [Fact]
+        public void AuthorizationTokenFail()
+        {
+            //Setup
+            var mockUrlPayloadRequest = new Mock<UrlPayload>();
+            var mockUrlHeaderService = new Mock<UrlActionService>();
+            var mockUrlFileDownloadService = new Mock<UrlFileDownloadService>();
+
+            //Sets up the url field to return google.com
+            mockUrlPayloadRequest.Setup(s => s.Url).Returns("http://releases.ubuntu.com/19.04/ubuntu-19.04-desktop-amd64.iso.torrent");
+
+            var controller = new TorrentFileController(mockUrlHeaderService.Object);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.Request.Headers.Add("Authorization", "false");
+
+            //Act
+            var result = controller.PostUrlPayload(mockUrlPayloadRequest.Object);
+
+            //Assert that it is of the type GuidResult
+            Assert.IsType<ActionResult<GuidResult>>(result);
+
+            //Assert that the Guid Payload string is not empty, as the url does contain a torrent file
+            Assert.True(String.IsNullOrEmpty(result.ToString()));
+        }
     }
 }
