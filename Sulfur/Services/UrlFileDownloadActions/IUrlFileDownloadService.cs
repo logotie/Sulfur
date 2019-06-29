fdced8aff834f9cd6603aaaf134a872e3f4732d3
Phelps <phelpsakinsan@gmail.com>
2019-06-29 19:34:29 +0100

    Added service and interface

---
+++ Sulfur/Services/UrlFileDownloadActions/IUrlFileDownloadService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sulfur.Services.UrlFileDownloadActions
+{
+    public interface IUrlFileDownloadService
+    {
+    }
+}
