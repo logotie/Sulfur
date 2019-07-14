d69bd89c5cb45717591da94101eb95c07bcacbb0
logotie <phelpsakinsan@gmail.com>
2019-07-14 23:08:22 +0100

    Working on the tests

--- SulfurXunitTests/UrlTorrentServiceTests.cs
+++ SulfurXunitTests/UrlTorrentServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Moq;
+using Sulfur.Services.UrlTorrentDownloadActions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,6 +9,13 @@ namespace SulfurXunitTests
 {
     public class UrlTorrentServiceTests
     {
+        private readonly IUrlTorrentService urlTorrentService;
+        public UrlTorrentServiceTests()
+        {
+            var mock = new Mock<UrlTorrentService>();
+            urlTorrentService = mock.Object;
+        }
+
         [Fact]
         public void TestWhetherTorrentContainsAnyAudioFiles()
         {
